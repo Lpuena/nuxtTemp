39db87dd68884b7526417d84b4b04182be4d4618
Lpuena <lpuena@outlook.com>
2025-01-05 16:06:49 +0800

    feat: add unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,28 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@nuxt/eslint',
+    '@unocss/nuxt',
   ],
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  css: [
+    '@unocss/reset/tailwind.css',
+  ],
   eslint: {
     config: {
       standalone: false,
     },
   },
+  app: {
+    head: {
+      viewport: 'width=device-width,initial-scale=1',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
 })
